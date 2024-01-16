cd698ea2f34ab2ff694cc6811524738e6de84fbb
Kseniya <k.nemtyryova@gmail.com>
2024-01-16 11:25:39 +0300

    script for lists

--- src/js/initLists.js
+++ src/js/initLists.js
@@ -1,20 +1,29 @@
 export function initLists(){
                   
-        document.querySelectorAll('.prices-content__1lv-list li').forEach(el =>{
-            el.addEventListener('click', ()=>{
+    document.querySelectorAll('.prices-content__item-title').forEach(el=>{
+        el.addEventListener('click', ()=>{
 
-                el.querySelectorAll('ul').forEach(list =>{
-
-                if(list.classList.contains('close')){
+            if (el.nextElementSibling.childElementCount == 0) {
+                const list = el.nextElementSibling.nextElementSibling;
+                if (list.classList.contains('close')) {
                     el.querySelector('.prices-content__plus').innerHTML = '-'
                     list.classList.replace("close", "open");
-                }
-                else{
+                } else {
                     list.classList.replace("open", "close");
                     el.querySelector('.prices-content__plus').innerHTML = '+'
                 }
-                
-            })
-        }) 
-    }) 
+            } else {
+                const list = el.nextElementSibling;
+                if (list.classList.contains('close')) {
+                    el.querySelector('.prices-content__plus').innerHTML = '-'
+                    list.classList.replace("close", "open");
+                } else {
+                    list.classList.replace("open", "close");
+                    el.querySelector('.prices-content__plus').innerHTML = '+'
+                }
+            }
+
+        }
+        )
+    })
 }
